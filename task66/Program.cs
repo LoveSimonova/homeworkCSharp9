e99f40b8ab4c8797ff30cccea24a5fcc5edf909e
«SimLove» <simonolyuba@yandex.ru>
2023-06-23 14:45:18 +0300

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int GetMinNumber()
+{
+    Console.WriteLine("Введите наименьшее число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetMaxNumber()
+{
+    Console.WriteLine("Введите наибольшее число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumRec(int min, int num)
+{
+    if(num==min) 
+        return min;    
+    else 
+        return num+SumRec(min,num-1);
+}
+
+int m=GetMinNumber();
+int n=GetMaxNumber();
+Console.Write("Сумма: " + SumRec(m,n));
