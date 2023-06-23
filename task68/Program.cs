5c22651f22d2b732d10f3c72f2720c1f41fbfb11
«SimLove» <simonolyuba@yandex.ru>
2023-06-23 15:16:23 +0300

    add task68

---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AckRec(int m, int n)
+{
+    if(m==0) 
+        return n+1;    
+    else if((m>0)&&(n==0))    
+        return AckRec(m-1,1);    
+    else
+        return AckRec(m-1,AckRec(m,n-1));
+}
+
+int m=GetNumber();
+int n=GetNumber();
+Console.WriteLine("А(m,n): " +AckRec(m,n));
