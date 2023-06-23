da89c71f2f54fc75d102d3db66f9c8e5febf897c
«SimLove» <simonolyuba@yandex.ru>
2023-06-23 14:35:12 +0300

    add task64

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int OutputNumRec(int num)
+{
+    if(num==1) 
+    {
+        Console.Write(""+num);
+        return 1;
+    }
+    else 
+    {
+        Console.Write(""+num+" ");
+        return num*OutputNumRec(num-1);
+    }
+}
+
+int n=GetNumber();
+Console.WriteLine("Числа: " );
+OutputNumRec(n);
